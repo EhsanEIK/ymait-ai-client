22df416e1b0534f10d634cf6d2deccf8c1d82ad5
EhsanEIK <ehsankhan1997.ek@gmail.com>
2025-03-27 09:44:45 +0600

    added handleSubmitPCData function to save the data from form to db

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 import "react-datepicker/dist/react-datepicker.css";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Toaster />
     </>
   )
 }
--- src/components/PC/AddPC.jsx
+++ src/components/PC/AddPC.jsx
@@ -1,49 +1,85 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import DatePicker from 'react-datepicker';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 
 const AddPC = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    const [serviceDate, setServiceDate] = useState(new Date());
+    const [purchaseDate, setPurchaseDate] = useState(new Date());
+    const [lastServiceDate, setLastServiceDate] = useState(new Date());
+
+    const handleSubmitPCData = event => {
+        event.preventDefault();
+        const form = event.target;
+        const itemType = form.itemType.value;
+        const purchaseDate = form.purchaseDate.value;
+        const brandName = form.brandName.value;
+        const model = form.model.value;
+        const supplierName = form.supplierName.value;
+        const assetCode = form.assetCode.value;
+        const userName = form.userName.value;
+        const email = form.email.value;
+        const department = form.department.value;
+        const extNumber = form.extNumber.value;
+        const lastServiceDate = form.lastServiceDate.value;
+
+        const pcData = {
+            itemType, purchaseDate, brandName, model, supplierName, assetCode,
+            userName, email, department, extNumber,
+            lastServiceDate
+        }
+
+        fetch('http://localhost:5000/pcData', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(pcData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success('Added PC Data Successfully.');
+            })
+    }
+
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmitPCData}>
                 {/* PC Info Part */}
                 <div>
                     <h2 className='text-2xl font-bold text-teal-500 uppercase'>PC Info</h2>
                     <hr />
                     <div className='flex gap-5 mt-3'>
-                        <select className="select mb-3">
+                        <select name='itemType' className="select mb-3">
                             <option defaultValue={true}>-- SELECT PC TYPE --</option>
                             <option>Desktop</option>
                             <option>Laptop</option>
                         </select>
                         <label className="input input-bordered flex items-center z-10 gap-2 mb-3">
                             Purchase Date
-                            <DatePicker selected={startDate}
-                                onChange={(date) => setStartDate(date)}
+                            <DatePicker name='purchaseDate' selected={purchaseDate}
+                                onChange={(date) => setPurchaseDate(date)}
                             />
                         </label>
                     </div>
                     <div className='flex gap-5'>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Brand
-                            <input type="text" className="grow" placeholder="brand name" />
+                            <input name='brandName' type="text" className="grow" placeholder="brand name" />
                         </label>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Model
-                            <input type="text" className="grow" placeholder="model" />
+                            <input name='model' type="text" className="grow" placeholder="model" />
                         </label>
                     </div>
                     <div className='flex gap-5'>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Supplier Name
-                            <input type="text" className="grow" placeholder="supplier name" />
+                            <input name='supplierName' type="text" className="grow" placeholder="supplier name" />
                         </label>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Asset Code
-                            <input type="text" className="grow" placeholder="asset code" />
+                            <input name='assetCode' type="text" className="grow" placeholder="asset code" />
                         </label>
                     </div>
                 </div>
@@ -55,27 +91,27 @@ const AddPC = () => {
                     <div className='flex gap-5 mt-3'>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             User Name
-                            <input type="text" className="grow" placeholder="user name" />
+                            <input name='userName' type="text" className="grow" placeholder="user name" />
                         </label>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Email
-                            <input type="text" className="grow" placeholder="email" />
+                            <input name='email' type="text" className="grow" placeholder="email" />
                         </label>
                     </div>
                     <div className='flex gap-5'>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Dept.
-                            <input type="text" className="grow" placeholder="department" />
+                            <input name='department' type="text" className="grow" placeholder="department" />
                         </label>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Office ID
-                            <input type="text" className="grow" placeholder="id no." />
+                            <input name='officeID' type="text" className="grow" placeholder="id no." />
                         </label>
                     </div>
                     <div className='flex gap-5'>
                         <label className="input input-bordered flex items-center gap-2 mb-3">
                             Extension Number
-                            <input type="text" className="grow" placeholder="ext. number" />
+                            <input name='extNumber' type="text" className="grow" placeholder="ext. number" />
                         </label>
                     </div>
                 </div>
@@ -87,8 +123,8 @@ const AddPC = () => {
                     <div className='flex gap-5 mt-3'>
                         <label className="input input-bordered flex items-center z-10 gap-2 mb-3">
                             Last Service Date
-                            <DatePicker selected={serviceDate}
-                                onChange={(date) => setServiceDate(date)}
+                            <DatePicker name='lastServiceDate' selected={lastServiceDate}
+                                onChange={(date) => setLastServiceDate(date)}
                             />
                         </label>
                     </div>
